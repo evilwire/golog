cab0204c2f696535578b0f76798a4caf3ad91014
Knight Fu <knight.w.fu@gmail.com>
2016-11-04 15:52:13 -0400

    adds documentation for levels

--- levels.go
+++ levels.go
@@ -1,6 +1,4 @@
-// Logging levels
-//
-//
+// Package representing Logging levels (enum type)
 package golog
 
 import (
@@ -8,18 +6,36 @@ import (
 	"strings"
 )
 
-
+// An alias for glog.Level, declares the enum type but restricts the user
+// from being able to create instances
 type level glog.Level
 
+// Enum representing log levels
 const (
+
+	// do not log except fatal
 	NOLOG = level(-1)
+
+	// logging only errors
 	ERROR = level(1)
+
+	// logging errors and warning
 	WARN = level(2)
+
+	// first level of text-based logging
 	VERBOSE = level(3)
+
+	// second level of text-based logging
 	INFO = level(4)
+
+	// third and last level of text-based logging
 	DEBUG = level(5)
 )
 
+// Retrieve a certain level by name, and if the name is not recognised returns NOLOG,
+// false to indicate that the level is not recognized.
+//
+// levelName (string) - the name of the level
 func GetLevel(levelName string) (level, bool) {
 	levelName = strings.ToUpper(levelName)
 
